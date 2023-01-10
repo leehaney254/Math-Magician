33cd6372346f29af26319d8ed52f7b7459b4e948
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-10 17:13:58 +0300

    Style: Create complete UI

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,37 +6,37 @@ class Calculator extends React.Component {
   render() {
     return (
       <main id="main">
-        <div className="height">
-          <p>0</p>
+        <div className="height" id="answer">
+          <p id="spaceright">0</p>
         </div>
         <div className="height">
           <button type="button" className="button">AC</button>
           <button type="button" className="button">+/-</button>
           <button type="button" className="button">%</button>
-          <button type="button" className="button">&divide;</button>
+          <button type="button" className="button symbols">&divide;</button>
         </div>
         <div className="height">
           <button type="button" className="button">7</button>
           <button type="button" className="button">8</button>
           <button type="button" className="button">9</button>
-          <button type="button" className="button">&#215;</button>
+          <button type="button" className="button symbols">&#215;</button>
         </div>
         <div className="height">
           <button type="button" className="button">4</button>
           <button type="button" className="button">5</button>
           <button type="button" className="button">6</button>
-          <button type="button" className="button">-</button>
+          <button type="button" className="button symbols">-</button>
         </div>
         <div className="height">
           <button type="button" className="button">1</button>
           <button type="button" className="button">2</button>
           <button type="button" className="button">3</button>
-          <button type="button" className="button">+</button>
+          <button type="button" className="button symbols">+</button>
         </div>
         <div className="height">
           <button type="button" className="button1">0</button>
           <button type="button" className="button">.</button>
-          <button type="button" className="button">=</button>
+          <button type="button" className="button symbols">=</button>
         </div>
       </main>
     );
