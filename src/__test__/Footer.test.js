6c2c0e4c576ed9beb2cb3c75426dea916615f834
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-20 12:17:27 +0300

    Feat: Add all tests

---
+++ src/__test__/Footer.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Footer from '../components/Footer';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Footer />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
